18f2487c8366081c87938cf34d8809ddaf082318
kimyong8175 <kimyong8175@naver.com>
2023-10-04 14:40:19 +0900

    created order controller and routes

--- backend/controllers/orderContoller.js
+++
@@ -1,6 +0,0 @@
-import asyncHandler from "../middleware/asyncHandler.js";
-import Order from "../models/orderModel.js";
-
-// @desc    Create new order
-// @route   POST /api/orders
-// @access  Private
