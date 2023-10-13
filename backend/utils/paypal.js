1a1fb93821ee027dc89073073c771e96ed9d4d6f
kimyong8175 <kimyong8175@naver.com>
2023-10-14 00:06:15 +0900

    modified env variable

--- backend/utils/paypal.js
+++ backend/utils/paypal.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 dotenv.config();
-const { PAYPAL_CLIENT_ID, PAYPAL_API_SECRET, PAYPAL_API_URL } = process.env;
+const { PAYPAL_CLIENT_ID, PAYPAL_APP_SECRET, PAYPAL_API_URL } = process.env;
 
 /**
  * Fetches an access token from the PayPal API.
@@ -12,7 +12,7 @@ const { PAYPAL_CLIENT_ID, PAYPAL_API_SECRET, PAYPAL_API_URL } = process.env;
  */
 async function getPayPalAccessToken() {
   // Authorization header requires base64 encoding
-  const auth = Buffer.from(PAYPAL_CLIENT_ID + ":" + PAYPAL_API_SECRET).toString(
+  const auth = Buffer.from(PAYPAL_CLIENT_ID + ":" + PAYPAL_APP_SECRET).toString(
     "base64"
   );
 
